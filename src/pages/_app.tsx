b7db77fac36ffc32f1eaa250777f461cb06ae55b
hunnu029 <shutachimura@gmail.com>
2021-04-22 01:42:31 +0900

    attach global styles

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import type { AppProps } from "next/app";
+import "ress/dist/ress.min.css";
+import { injectGlobal } from "@emotion/css";
+
+injectGlobal`
+  html, body, #__next {
+      min-height: 100%;
+  }
+
+  body {
+      background: #29292b;
+      font-family: "Helvetica Neue",
+        Arial,
+        "Hiragino Kaku Gothic ProN",
+        "Hiragino Sans",
+        Meiryo,
+        sans-serif;
+  }
+`;
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
