7c5bc48b608250f5d0578fb40b5076f6b4cc4bd9
hunnu029 <shutachimura@gmail.com>
2021-04-22 01:43:44 +0900

    add header index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import { css } from "@emotion/css";
 import { NextPage } from "next";
+import { Header } from "../components/Header";
 
+//IndexPageというNextPage(Reactコンポーネントの拡張版)を宣言、初期化している。
 const IndexPage: NextPage = () => {
-  return <p> hoge</p>;
+  return <Header />;
 };
 
+//IndexPageをNext.jsに渡して、実際に表示させてる
 export default IndexPage;
