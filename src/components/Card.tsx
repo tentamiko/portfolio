ee1170375234dad1d9cbddd6899fcb1bd0b07666
hunnu029 <shutachimura@gmail.com>
2021-05-18 00:27:30 +0900

    change Card style when hover

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,17 +7,61 @@ type Props = { pictureUrl: string; linkUrl: string; title: string };
 export const Card: VFC<Props> = ({ pictureUrl, linkUrl, title }) => {
   return (
     <Link href={linkUrl}>
-      <a>
+      <a
+        className={css`
+          text-decoration: none;
+        `}
+      >
         <article
           className={css`
-            background-image: url(${pictureUrl});
-            background-size: cover;
-            background-position: center;
+            position: relative;
             width: 100%;
             height: 100%;
           `}
         >
-          <h1>{title}</h1>
+          <img
+            src={pictureUrl}
+            alt=""
+            className={css`
+              z-index: -1;
+              position: absolute;
+              top: 0;
+              left: 0;
+              width: 100%;
+              height: 100%;
+
+              object-fit: cover;
+              object-position: center;
+
+              transition: 0.15s filter;
+
+              a:focus &,
+              a:hover & {
+                filter: brightness(0.4);
+              }
+            `}
+          />
+          <h1
+            className={css`
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              width: 100%;
+              height: 100%;
+              opacity: 0;
+              transition: 0.15s opacity;
+              text-decoration: none;
+              font-size: 2rem;
+              color: #ffffff;
+
+              a:focus &,
+              a:hover & {
+                opacity: 1;
+              }
+            `}
+          >
+            {title}
+          </h1>
         </article>
       </a>
     </Link>
